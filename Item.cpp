4cb77babbb691d7d82f96e4f64a5a664730e1a8b
LeeMinHo <alsghsla1227@gmail.com>
2020-10-26 03:23:14 +0900

    Item

--- Item.cpp
+++ Item.cpp
@@ -2,38 +2,23 @@
 
 const string Item::namelist[4] = {"목검", "장검", "HP포션", "MP포션"}; // 번호에 따른 아이템 이름들
 vector<vector<int>> itemlist;
-//itemarr[4][3] = {{1,100,300},{1,200,500},{0,100,100},{0,100,100}};
-Item::Item()
-        : itemNum(0), itemType(0), itemValue(1), itemPrice(0) {
-
+vector<int> itemlist1{1, 200, 200};
+vector<int> itemlist2{1, 400, 400};
+vector<int> itemlist3{0, 100, 100};
+vector<int> itemlist4{0, 100, 100};
+Item::Item() {
+    itemlist.push_back(itemlist1);
+    itemlist.push_back(itemlist2);
+    itemlist.push_back(itemlist3);
+    itemlist.push_back(itemlist4);
 }
-
-Item::Item(const int &i_itemNum, const int &i_itemType, const int &i_itemValue, const int &i_itemPrice) {
-    this->itemNum = i_itemNum;
-    this->itemType = i_itemType;
-    this->itemPrice = i_itemPrice;
-    this->itemValue = i_itemValue;
-
-    vector<int> itemattribute;
-    itemlist[this->itemNum] = itemattribute;
-
-    itemattribute.push_back(this->itemPrice);
-    itemattribute.push_back(this->itemType);
-    itemattribute.push_back(this->itemValue);
-}
-
 Item::~Item() {}
-
 int Item::get_itemNum() const {
     return this->itemNum;
 }
-
 vector<int> Item::get_item(int i_num) const {
-
-
-//    return this->itemattribute;
-    return this->itemlist[i_num];
-
+//  return this->itemattribute;
+    return this->itemlist[i_num - 1];
 }
 
 string Item::get_itemName(const int &number) const {
--- Item.h
+++ Item.h
@@ -6,28 +6,26 @@
 using namespace std;
 
 class Item {
-    private:
-        static const string namelist[4]; // 아이템 번호에 따른 이름들
-        static vector<vector<int>> itemlist;
-        int itemNum;
-        int itemType;
-        int itemValue;
-        int itemPrice;
-        //vector<int> itemattribute; // 아이템 속성들을 담는 벡터
-        int itemarr[][];
+private:
+    static const string namelist[4]; // 아이템 번호에 따른 이름들
+    vector<vector<int>> itemlist;
+    vector<int> itemlist1;
+    vector<int> itemlist2;
+    vector<int> itemlist3;
+    vector<int> itemlist4;
+    int itemNum;
 
-    public:
-        Item();
-        Item(const int& itemNum, const int& itemType, const int& itemValue, const int& itemPrice);
-        ~Item();
+public:
+    Item();
+    ~Item();
 
-        int get_itemNum() const;
-        vector<int> get_item(int i_num) const;
-        string get_itemName(const int& number) const;
-        
-        
-        // 우선은 getset 같은 경우는 전체를 한번에 수정, 반환하는 형식으로 구현, 이후 특정 변수만 반환, 수정하고 싶다면
-        //추가 예정
+    int get_itemNum() const;
+    vector<int> get_item(int i_num) const;
+    string get_itemName(const int& number) const;
+
+
+    // 우선은 getset 같은 경우는 전체를 한번에 수정, 반환하는 형식으로 구현, 이후 특정 변수만 반환, 수정하고 싶다면
+    //추가 예정
 
 
 };
