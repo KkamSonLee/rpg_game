10d1c597476fc6fb2fc16ff939bf23ac86980b91
IslandofDream <70648111+IslandofDream@users.noreply.github.com>
2020-10-26 00:32:21 +0900

    Add files via upload

--- Character.h
+++ Character.h
@@ -1,30 +1,26 @@
 #pragma once
 #include<iostream>
 #include <vector>
+#include "Monster.h"
 #include "Status.h"
 
 
 class Character
 { 
     private:
-        Status status();
-        int slot[10];
+        Status status;
     
     public:
         Character(Status status);
         ~Character();
         void show_stat();
-        void attack();
-
         Status get_stat();
-        void set_stat();
-        int get_slot();
-        void set_slot(int num);
-        void sort_slot();
+        void set_stat(int& i_max_HP, int& i_max_MP, int& i_now_HP,int& i_now_MP, int& i_atk, int& i_exp, int& i_level, int& i_location);
+        void set_atk(int i_atk);
+        int get_atk;
+        void attack(Monster mon);
         
         //Inventory.open();
-        // get set ÇÔ¼öµé
-        // 
 
 
 };
--- Status.cpp
+++ Status.cpp
@@ -12,13 +12,20 @@ Status::Status()
     this -> exp = 0;
     this -> level = 1;
     this -> location = 0;
-    this -> money = 0;            
     // 기획서상 level은 1이상의 숫자 나머지는 0이상의 숫자
 }
 
-Status::Status(int& max_HP, int& max_MP, int& now_HP,int& now_MP, int& atk, int& exp, int& level, int& location, int& money)
-    :max_HP(max_HP),max_MP(max_MP),now_HP(now_HP),now_MP(now_MP),atk(atk),exp(exp),level(level),location(location), money(money) 
+Status::Status(int& i_max_HP, int& i_max_MP, int& i_now_HP,int& i_now_MP, int& i_atk, int& i_exp, int& i_level, int& i_location)
 {
+    this->max_HP = i_max_HP;
+    this-> max_HP = i_max_MP;
+    this -> now_HP = i_now_HP;
+    this -> now_MP = i_now_MP;
+    this -> atk = i_atk;
+    this -> exp = i_exp;
+    this -> level = i_level;
+    this -> location = i_location;     
+
     stat_list.push_back(max_HP);
     stat_list.push_back(max_MP);
     stat_list.push_back(now_HP);
@@ -27,7 +34,6 @@ Status::Status(int& max_HP, int& max_MP, int& now_HP,int& now_MP, int& atk, int&
     stat_list.push_back(exp);
     stat_list.push_back(level);
     stat_list.push_back(location);
-    stat_list.push_back(money);
 }
 
 Status::~Status()
@@ -37,12 +43,57 @@ Status::~Status()
 
 void Status::show()
 {
-    cout << max_HP << max_MP << now_HP << now_MP << atk << exp << level << money << endl;
+    cout << max_HP << max_MP << now_HP << now_MP << atk << exp << level << endl;
+}
+
+void Status::setstatus(int& i_max_HP, int& i_max_MP, int& i_now_HP,int& i_now_MP, int& i_atk, int& i_exp, int& i_level, int& i_location)
+{
+    this->max_HP = i_max_HP;
+    this-> max_HP = i_max_MP;
+    this -> now_HP = i_now_HP;
+    this -> now_MP = i_now_MP;
+    this -> atk = i_atk;
+    this -> exp = i_exp;
+    this -> level = i_level;
+    this -> location = i_location;
 }
 
 vector<int> Status::getstatus()
 {
-   
-
     return this->stat_list;
+}
+
+void Status::set_atk(int i_atk)
+{
+    this->atk = i_atk;
+}
+void Status::setn_hp(int i_hp)
+{
+    this -> now_HP = i_hp;
+}
+
+void Status::setn_mp(int i_mp)
+{
+    this -> now_MP = i_mp;
+}
+
+void Status::setn_mhp(int i_hp, int i_mp)
+{
+    this -> now_HP = i_hp;
+    this -> now_MP = i_mp;
+}
+
+int Status::getn_hp()
+{   
+    return this->now_HP;
+}
+
+int Status::getn_mp()
+{
+    return this->now_MP;
+}
+
+int Status::getn_mp()
+{
+    return this->atk;
 }
