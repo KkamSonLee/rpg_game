d80a958a4da1b623687fd0a720070ff6ff51fa81
GichuL0602 <57994477+GichuL0602@users.noreply.github.com>
2020-10-27 01:59:30 +0900

    Add files via upload
    so tired

---
+++ battle.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class battle
+{
+public:
+	int Battle(Character& myCharacter, Inventory& myInventory, Monster& nowMonster, int map_num); //전투에 임하는 함수
+	void show_Stats(Character& myCharacter, Monster& nowMonster); //스탯 보여주는 함수
+};
+
