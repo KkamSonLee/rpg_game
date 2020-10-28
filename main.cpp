02a16771144c848fa8cf3b2c9c9e34c2b79bc986
JunWooKim <rhffldkt0517@naver.com>
2020-10-28 20:43:57 +0900

    fix!!!!!!1

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "Character.h"
 #include <conio.h>
 int main() {
+    int use_mp = 20;
     int num = 1;
     int hp = 3;
     int mp = 100;
@@ -12,12 +13,12 @@ int main() {
     Monster mon(st, 1);
     ch.show_stat();// 스탯 정보 출력
 
-    ch.attack(mon, 10);
+    ch.attack(mon, mp);
     cout << mon.get_nhp() << endl; //// hp = 3 , atk = 1 이기때문에 공격을 한번 한다면 hp 2가 되어야 할 것 입니다.
 
-    ch.attack(mon, 10);
-    mon.attack(ch, 10);
-    mon.attack(ch, 10);
+    ch.attack(mon, mp);
+    mon.attack(ch, mp);
+    mon.attack(ch, mp);
     // 캐릭터와 몬스터의 status가 똑같고 mp 소모량도 20이기 때문에 
     //캐릭터와 몬스터의 hp mp는 두번 공격했을 때 1 -37이 나와야 합니다. 
     cout << mon.get_nhp() << "," << mon.get_nmp() << endl;
