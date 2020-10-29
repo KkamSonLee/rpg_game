cc2d5c1bbe28a33dc0ebeb7d33403f6ff2a7255b
yoobyungchan <fkdn1234@naver.com>
2020-10-29 20:20:46 +0900

    change array to vector and save warning map file

--- map_integrity_check.cpp
+++ map_integrity_check.cpp
@@ -1,4 +1,8 @@
 #include "map_integrity_check.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 #define MAP_NUM 10
@@ -9,19 +13,51 @@ enum {
 	ERROR,
 	WARNING
 };
-map_integrity_check::map_integrity_check(){
 
+
+map_integrity_check::map_integrity_check()
+{
 }
+
 map_integrity_check::~map_integrity_check()
 {
 }
 
+void map_integrity_check::save_change(string s, vector<int> val1, vector<int> val2)
+{
+	ofstream sfile(s);
+
+	if (sfile.is_open()) {
+		for (int i = 0; i < val1.size(); i++) {
+			if (val1[i] < 0) {
+				break;
+			}
+			sfile << val1[i];
+			sfile << "\t";
+
+		}
+		sfile << "\n";
+		for (int i = 0; i < val2.size(); i++) {
+			if (val2[i] < 0) {
+				break;
+			}
+			sfile << val2[i];
+			sfile << "\t";
+
+		}
+		sfile.close();
+
+	}
+}
+
+
 int map_integrity_check::load_set(string s)
 {
-	int check_value_1[10] = { -1,-1,-1,-1,-1,-1,-1,-1,-1,-1 };
-	int check_value_2[9] = { -1,-1,-1,-1,-1,-1,-1,-1,-1 };
+	vector<int> check_value_1;
+	vector<int> check_value_2;
 	char datas_1[100];
 	char datas_2[100];
+	int Flag;
 	vector<string> check_list_1;
 	vector<string> check_list_2;
 
@@ -64,7 +100,7 @@ int map_integrity_check::load_set(string s)
 					return ERROR;
 				}
 			}
-			check_value_1[i] = stoi(check_list_1[i]);
+			check_value_1.push_back(stoi(check_list_1[i]));
 		}
 		catch (...) {
 			return ERROR;
@@ -73,13 +109,14 @@ int map_integrity_check::load_set(string s)
 	//오류 문자 존재하는지 체크
 	for (int i = 0; i < list_size_2; i++) {
 		try {
-			check_value_2[i] = stoi(check_list_2[i]);
+			check_value_2.push_back(stoi(check_list_2[i]));
 		}
 		catch (...) {
 			return ERROR;
 		}
 	}
 	//중복 맵 존재
+	int cnt = 0;
 	for (int i = 0; i < MAP_NUM; i++) {
 		if (check_value_1[0] == map_list[i]) {
 			cnt++;
@@ -106,12 +143,21 @@ int map_integrity_check::load_set(string s)
 	}
 	//마을에 몬스터 존재
 	if (check_value_2[1] == 1 && check_value_2[2] != 0) {
+		check_value_2[2] = 0;
+
+		save_change(s, check_value_1, check_value_2);
+
 		return WARNING;
 	}
 	//던전에 아이템 존재
-	if ((check_value_2[1] == 2) || (check_value_2[1] == 3) && (check_value_2[3] != 0)) {
+	if (((check_value_2[1] == 2) || (check_value_2[1] == 3)) && (check_value_2[3] != 0)) {
+		check_value_2[3] = 0;
+
+		save_change(s, check_value_1, check_value_2);
+
 		return WARNING;
 	}
 
 	return SUCCESS;
 }
+
