5578c55adebed729eecc730c8a0d066d9b08caa4
yoobyungchan <48464681+yoobyungchan@users.noreply.github.com>
2020-10-30 20:57:59 +0900

    add warning msg
    add warning msg

--- Map.cpp
+++ Map.cpp
@@ -14,6 +14,7 @@ void Map::load_map(string s)
 	vector<int> load_datas;
 	vector<int> temp_linked_map;
 	vector<int> temp_item_list;
+	warningMessage msg;
 	int Flag;
 	load_datas = check.load_set(s);
 	Flag = load_datas[0];
@@ -34,9 +35,13 @@ void Map::load_map(string s)
 			temp_item_list.push_back(load_datas[j]);
 		}
 		set_item_list(temp_item_list);
+		if (Flag == 2) {
+			msg.printWarning(1, 1);
+		}
 	}
 	else                    //error
 	{
+		msg.printWarning(0, 1);
 		return;
 	}
 }
