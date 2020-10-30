aa261da9970703fc3d19a1e51f28b69896bbaef7
GichuL0602 <57994477+GichuL0602@users.noreply.github.com>
2020-10-30 12:15:59 +0900

    so tired

--- battle.h
+++ battle.h
@@ -13,8 +13,8 @@ using namespace std;
 
 class battle{
 public:
-	int Battle(Character& myCharacter, Inventory& myInventory, Monster& nowMonster, int map_num); //������ ���ϴ� �Լ�
-	void show_Stats(Character& myCharacter, Monster& nowMonster); //���� �����ִ� �Լ�
-	void save_character(Character& myCharacter, Inventory& myInventory); //ĳ���� ������ �����ϴ� �Լ�
-	int attack_situation(Character& myCharacter, Inventory& myInventory, Monster& nowMonster, int map_num); //attack ���� ���� ��Ȳ�� ��Ÿ�� �Լ�
+	int Battle(Character& myCharacter, Inventory& myInventory, Monster& nowMonster, int map_num, warningMessage& wMessage); 
+	void show_Stats(Character& myCharacter, Monster& nowMonster); 
+	void save_character(Character& myCharacter, Inventory& myInventory, warningMessage &wMessage); 
+	int attack_situation(Character& myCharacter, Inventory& myInventory, Monster& nowMonster, int map_num, warningMessage &wMessage); 
 };
